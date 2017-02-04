6b0a322225a711614d1df592305c131aa7e15f14
handriss <andrashinkel@gmail.com>
2017-02-04 18:37:07 +0100

    client side socket workflow finished

--- tcp_client.c
+++ tcp_client.c
@@ -8,5 +8,28 @@
 
 int main(){
 
+    int network_socket;
+    network_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in server_address;
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+
+    if(connection_status == -1){
+        printf("Sorry buddy, there was an error making a connection!\n\n");
+    }
+
+    char server_response[256];
+    recv(network_socket, &server_response, sizeof(server_response), 0);
+
+    printf("The server sent the following data: %s\n", server_response);
+
+    close(network_socket);
+    
+
     return 0;
 }
